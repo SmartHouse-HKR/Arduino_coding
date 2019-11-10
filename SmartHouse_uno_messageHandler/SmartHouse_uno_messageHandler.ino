f0c77aabf754f7378a0257b3b290aaf841669f3a
Rasmus Eriksson <multi950rgmail.com>
2019-11-10 23:48:17 +0100

    updated wifi module to the correct file as well as created message handler method inside the uno file

--- SmartHouse_uno_messageHandler/SmartHouse_uno_messageHandler.ino
+++ SmartHouse_uno_messageHandler/SmartHouse_uno_messageHandler.ino
@@ -1,78 +1,81 @@
 #include <SoftwareSerial.h>
-SoftwareSerial softSerial(0,1);
- bool waitingMsg = false;
-  int messageSent = 0;
-  int pin1 = 8;
-  int pin2 = 11;
-  int pin3 = 12;
-  int pin4 = 13;
-  int fanPin = 13;
+SoftwareSerial wifiMessage(0,1);
+bool waitingMsg = false;
+int messageSent = 0;
+int pin1 = 8;
+int pin2 = 11;
+int pin3 = 12;
+int pin4 = 13;
+int fanPin = 13;
 
 
 void sendMessage(String message){
 
-  char charArray[50];
-  message.toCharArray(charArray, 20);
-  softSerial.write(charArray);
-  softSerial.write(messageSent++);
-  softSerial.write('\n');
+        char charArray[50];
+        message.toCharArray(charArray, 20);
+        wifiMessage.write(charArray);
+        wifiMessage.write(messageSent++);
+        wifiMessage.write('\n');
 }
 
-String receiveMessage(){ 
-  char part;
-  String message= "";
-    while(softSerial.available()){
-      part = ((char)softSerial.read());
-      if(part != '\n'){
-      message += part;    
-      }else if(message != "")
-        break;
-
-      delay(50);
+void messageHandler(String returnedMessage) {
+  if(returnedMessage.equals("ON")) {
+          turnOnLight();
+          digitalWrite(fanPin, HIGH);
+          Serial.println(returnedMessage);
+  }else if(returnedMessage.equals("OFF")) {
+          turnOffLight();
+          digitalWrite(fanPin, LOW);
+  }
 }
-return message;
+
+String receivedMessage(){
+        char part;
+        String message= "";
+        while(wifiMessage.available()) {
+                part = ((char)wifiMessage.read());
+                if(part != '\n') {
+                        message += part;
+                }else if(message != "")
+                        break;
+                delay(50);
+        }
+        return message;
 }
 
 void turnOnLight(){
-   digitalWrite(pin1, HIGH);
-  digitalWrite(pin2, HIGH);
-  digitalWrite(pin3, LOW);
-  digitalWrite(pin4, HIGH);
+        digitalWrite(pin1, HIGH);
+        digitalWrite(pin2, HIGH);
+        digitalWrite(pin3, LOW);
+        digitalWrite(pin4, HIGH);
 }
 void turnOffLight(){
-   digitalWrite(pin1, LOW);
-  digitalWrite(pin2, LOW);
-  digitalWrite(pin3, HIGH);
-  digitalWrite(pin4, HIGH);
+        digitalWrite(pin1, LOW);
+        digitalWrite(pin2, LOW);
+        digitalWrite(pin3, HIGH);
+        digitalWrite(pin4, HIGH);
 }
 
 void setup(){
 
-    pinMode(pin1, OUTPUT);
-    pinMode(pin2, OUTPUT);
-    pinMode(pin3, OUTPUT);
-    pinMode(pin4, OUTPUT);
-    pinMode(fanPin, OUTPUT);
-    
-turnOffLight();
-  
-  Serial.begin(9600);
-  softSerial.begin(4800);
+        pinMode(pin1, OUTPUT);
+        pinMode(pin2, OUTPUT);
+        pinMode(pin3, OUTPUT);
+        pinMode(pin4, OUTPUT);
+        pinMode(fanPin, OUTPUT);
+
+        turnOffLight();
+
+        Serial.begin(9600);
+        wifiMessage.begin(4800);
 
 }
 
 void loop(){
-  
-  if(softSerial.available()){
-    String returnedMessage = receiveMessage();
-    if(returnedMessage.equals("ON")){
-      turnOnLight();
-        digitalWrite(fanPin, HIGH);
-      Serial.println(returnedMessage);
-    }else if(returnedMessage.equals("OFF")){
-     turnOffLight();
-     digitalWrite(fanPin, LOW);
-    }
-  }
-  }
-  
+
+
+        if(wifiMessage.available()) {
+           messageHandler(receivedMessage());
+
+        }
+}
--- SmartHouse_wifi-module/SmartHouse_wifi-module.ino
+++ SmartHouse_wifi-module/SmartHouse_wifi-module.ino
@@ -1,46 +1,126 @@
-#include <SoftwareSerial.h>
+/////////////////////////////////
+// Generated with a lot of love//
+// with TUNIOT FOR ESP8266     //
+// Website: Easycoding.tn      //
+/////////////////////////////////
 #include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+#include <PubSubClient.h>
 
 SoftwareSerial softSerial(D2,D3);
- bool waitingMsg = false;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+String networkSSL = "Ras_the_Moose";
+String networkPassword = "12345678";
+String ipAddress = "194.47.28.11";
+uint16_t port = 1883;
+String message = "HEY";
+int ledPin = 13;
+char msgmqtt[50];
+
+
+void reconnectmqttserver() {
+        while (!client.connected()) {
+                Serial.print("Attempting MQTT connection...");
+                String clientId = "ESP8266Client-";
+                clientId += String(random(0xffff), HEX);
+                if (client.connect(clientId.c_str())) {
+                        Serial.println("connected");
+                        client.subscribe("/smarthouse/temp/state");
+                        client.subscribe("/smarthouse/light/state");
+                } else {
+                        Serial.print("failed, rc=");
+                        Serial.print(client.state());
+                        Serial.println(" try again in 5 seconds");
+                        delay(5000);
+                }
+        }
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+        String MQTT_DATA = "";
+        for (int i=0; i<length; i++) {
+                MQTT_DATA += (char)payload[i];
+        }
+        Serial.println("message received from topic: " + MQTT_DATA + ", Topic:  " + topic);
+        dataHandler(MQTT_DATA);
+}
 
 void setup(){
-	Serial.begin(9600);
-	softSerial.begin(4800);
-	pinMode(D2,INPUT);
-	pinMode(D3,OUTPUT);
-}
 
-void loop(){
-  
-  if(softSerial.available())
-    waitingMsg = true;
-  if(waitingMsg){
-  Serial.println(receiveMessage());
-   delay(2000);
-  sendMessage("confirmed");
-  waitingMsg = false;
+        Serial.begin(9600);
+        softSerial.begin(4800);
+        pinMode(D2,INPUT);
+        pinMode(D3,OUTPUT);
+        pinMode(D7, OUTPUT);
+        WiFi.disconnect();
+        delay(3000);
+        Serial.println("START");
+        WiFi.begin(networkSSL,networkPassword);
+        while ((!(WiFi.status() == WL_CONNECTED))) {
+                delay(300);
+                Serial.print("..");
+
+        }
+        Serial.println("Connected");
+        Serial.println("Your IP is");
+        Serial.println((WiFi.localIP().toString()));
+        client.setServer( "192.168.43.109", 1883);
+        client.setCallback(callback);
+
 }
+void dataHandler(String data){
+        if(data.equals("ON")) {
+                sendMessage("ON");
+        } else if(data.equals("OFF")) {
+                sendMessage("OFF");
+        }
+
 }
 
 void sendMessage(String message){
-  char charArray[50];
-  message.toCharArray(charArray, 20);
-  softSerial.write(charArray);
-  softSerial.write('\n');
+        char charArray[50];
+        message.toCharArray(charArray, 20);
+        softSerial.write(charArray);
+        softSerial.write('\n');
 }
 
-String receiveMessage(){
+String receivedArduinoMessage(){
+        char part;
+        String message= "";
+        while(softSerial.available()) {
+                part = ((char)softSerial.read());
+                if(part != '\n') {
+                        message += part;
+                }else if(message != "")
+                        break;
+                delay(50);
 
-  char part;    String message= "";
-    while(softSerial.available()){
-      part = ((char)softSerial.read());
-      if(part != '\n'){
-      message += part;    
-      }else if(message != "")
-        break;
-
-      delay(50);
+        }
+        return message;
 }
-return message;
+
+void arduinoHandler(String arduinoData){
+        Serial.println(arduinoData);
+}
+
+void loop()
+{
+
+        if (!client.connected()) {
+                reconnectmqttserver();
+        }
+        client.loop();
+        snprintf (msgmqtt, 50, message.c_str());
+        String msg = "boppa loppa";
+        msg.toCharArray(msgmqtt, 50);
+        client.publish("test", msgmqtt);
+        delay(5000);
+
+        if(softSerial.available())
+                arduinoHandler(receivedArduinoMessage());
+
+
 }
