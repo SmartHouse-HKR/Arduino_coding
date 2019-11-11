e35ce5bc77e93440c2c46aa40c4d9aee93b75994
Cassian <Louliett@gmail.com>
2019-11-11 10:21:42 +0100

    Create SmartHouse_switches.ino

---
+++ SmartHouse_switches/SmartHouse_switches.ino
@@ -0,0 +1,38 @@
+int fire = 2;
+int water = 4;
+int stove = 5; 
+int window = 6; 
+
+
+void setup(){
+
+pinMode(fire, INPUT);
+pinMode(water, INPUT);
+pinMode(stove, INPUT);
+pinMode(window, INPUT);
+  
+}
+
+
+
+void loop() {
+  
+
+
+void fire(){
+  
+}
+
+void water() {
+  
+}
+
+void stove() {
+  
+  }
+
+void window() {}
+  
+  
+  
+}
